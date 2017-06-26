2ec833cb048da6b112175f36113b3273519f292a
tsvet-elina <panayotova.tsvetelina@gmail.com>
2017-06-26 10:47:26 +0300

    Home/about page

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'welcome#home'
-
+  get '/grid', to: 'welcome#grid'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
