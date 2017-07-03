5685eb36f0f925499c0650dab514d8b5a3d89c98
Bogomil Bonchev <bobbybonchev@yahoo.com>
2017-07-03 06:28:05 +0000

    routes, final

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root                    'welcome#home'
   get     '/signup', to:  'users#new'
   get     '/login',  to:  'sessions#new'
-  get     'games',    to: 'games#create'
+  get     'games',   to:  'games#create'
   get     'ship',    to:  'ships#new'
   post    '/signup', to:  'users#create'
   post    '/login',  to:  'sessions#create'
