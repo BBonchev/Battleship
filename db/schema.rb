c04f9912173ae00fd1f1b5831951921c33290fcf
Stefan Georgiev <stefangeorgiev359@gmail.com>
2017-07-03 11:16:53 +0300

    deleted games.coffe

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,6 @@ ActiveRecord::Schema.define(version: 20170630114523) do
 
   create_table "boards", force: :cascade do |t|
     t.text     "grid"
-    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
